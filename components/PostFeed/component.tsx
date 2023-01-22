803788bd250c8e6b16c2a0c30569012068dd1e2d
pmita <panos.mita2@outlook.com>
2023-01-22 21:03:44 +0000

    ammending the postCard component

--- components/PostFeed/component.tsx
+++ components/PostFeed/component.tsx
@@ -31,20 +31,28 @@ const PostCard = ({ post }) => {
 
     return (
         <div className={styles.postCard}>
-            <div className={styles.postCardContent}>
-                <h4>{post.content}</h4>
-                <div className={styles.postCardCTA}>
-                    {/* <Link href={`/${post.username}/${post.slug}`}>
-                        Check me Out
-                    </Link> */}
-                    <button className='btn primary' onClick={onClick}>Check me Out</button>
+            <div className={styles.postCardHeader}>
+                <div className={styles.postCardAvatar}>
+                    <img src='/assets/icons/hacker.png' alt='user icon' />
+                </div>
+                <div className={styles.postCardUsername}>
+                    <h4>{post.username}</h4>
+                    <Link href={`/${post.username}`}>
+                        <h6>@{post.username}</h6>
+                    </Link>
                 </div>
             </div>
+            <div className={styles.postCardTitle}>
+                <h4>{post.title}</h4>
+            </div>
+            <div className={styles.postCardContent}>
+                <h4>{post.content}</h4>
+            </div>
             <div className={styles.postCardFooter}>
-                <Link href={`/${post.username}`}>
-                    <h6>@{post.username}</h6>
-                </Link>
-                <span>
+                <span className={styles.postCardHearts}>
+                    <img src="/assets/icons/heart.svg" alt="heart icon" />
+                </span>
+                <span className={styles.postCardComments}>
                     <img src="/assets/icons/heart.svg" alt="heart icon" />
                 </span>
             </div>
